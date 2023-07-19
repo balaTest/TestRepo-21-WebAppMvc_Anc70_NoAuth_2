4245c10210b1976c6d1b0c788516bc94b10e28fe
Bala Chirtsabesan <balaTest@live.com>
2023-07-19 07:34:28 -0700

    Add project files.

---
+++ WebAppMvc_Anc70_NoAuth_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppMvc_Anc70_NoAuth_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
